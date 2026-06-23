c67d59568f19b3a6983421ae9592bb91b8adb72b
Nicholas Travaglini <ntravaglini@icloud.com>
2026-06-23 01:14:08 -0400

    Day 01: Variables and Const Reassignment

---
+++ week-01/day-01-variables/5-variables.js
@@ -0,0 +1,9 @@
+const siteName = "Travaglini Photography";
+let pageCount = 20;
+let isLive = true;
+const siteLocation = "Winter Park";
+const contact = "thedigitalnick@gmail.com";
+
+console.log(siteName, pageCount, isLive, siteLocation, contact)
+
+siteLocation = "Orlando"; //this line will break because of reassignment
